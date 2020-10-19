71429bb718d3271faf2fc407543021cf7c367c28
EliottBICS <47846877+EliottBICS@users.noreply.github.com>
2020-10-19 15:35:50 +0200

    Create utilis.py

---
+++ utilis.py
@@ -0,0 +1,45 @@
+from djitellopy import Tello
+import cv2
+
+
+def initializeTello():
+    myDrone = Tello()
+    myDrone.connect()
+    myDrone.for_back_velocity = 0
+    myDrone.left_right_velocity = 0
+    myDrone.up_down_velocity = 0
+    myDrone.yaw_velocity = 0
+    myDrone.speed = 0
+    print(myDrone.get_battery())
+    myDrone.streamoff()
+    myDrone.streamon()
+    return myDrone
+
+def telloGetFrame(myDrone, w = 360, h = 240):
+    myFrame = myDrone.get_frame_read()
+    myFrame = myFrame.frame
+    img = cv2.resize(myFrame,(w,h))
+    return img
+
+def findFace(img):
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
+
+    myFaceListC = []
+    myFaceListArea = []
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+        cx = (x+w)//2
+        cy = (y+h)//2
+        area = w*h
+        myFaceListArea.append(area)
+        myFaceListC.append([cx,cy])
+
+    if len(myFaceListArea) !=0:
+
+        i = myFaceListArea.index(max(myFaceListArea))
+        return img, [myFaceListC[i],myFaceListArea[i]]
+    else :
+        return img,[[0,0],0]
