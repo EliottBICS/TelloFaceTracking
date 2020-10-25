fb5ce8614a9cc7d5fc04a1ba47ef2f7e43815c59
EliottBICS <47846877+EliottBICS@users.noreply.github.com>
2020-10-25 09:55:23 +0100

    Update TelloFaceTracking.py

--- TelloFaceTracking.py
+++ TelloFaceTracking.py
@@ -4,7 +4,7 @@ import cv2
 w,h = 360,240
 
 
-myDrone = initializeTello()
+myDrone = initializeTello() #it initialises the drone, meaning we now have a drone with its feed perceived, a set speed (0) and a battery level
 
 while True:
     #step 1
@@ -12,7 +12,7 @@ while True:
 
     #step 2
     img, info = findFace(img)
-    print(info[0][0])
+    print(info[0][0]) #prints the first element of the tuple representing the center of the face (it will be its cx value)
 
     cv2.imshow('Image',img)
     if cv2.waitKey(1) & 0xFF == ord('q'): #if the Q key is pressed
