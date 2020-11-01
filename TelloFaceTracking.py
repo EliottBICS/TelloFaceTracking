0453ccd096c5c9e81a962ce16363031d28ec2366
EliottBICS <47846877+EliottBICS@users.noreply.github.com>
2020-11-01 13:43:57 +0100

    Update TelloFaceTracking.py

--- TelloFaceTracking.py
+++ TelloFaceTracking.py
@@ -2,16 +2,28 @@ from utilis import *
 import cv2
 
 w,h = 360,240
+pid = [0.5,0.5,0] # [kp, kd, ki]
+pError = 0
+startCounter = 0 #To stay on ground set to 1, to take off, set to 0
 
 
 myDrone = initializeTello() #it initialises the drone, meaning we now have a drone with its feed perceived, a set speed (0) and a battery level
 
 while True:
+    
+    #Take off
+    if startCounter == 0:
+        myDrone.takeoff()
+        startCounter = 1
+        
     #step 1
     img = telloGetFrame(myDrone,w,h)
 
     #step 2
     img, info = findFace(img)
+    
+    #step 3
+    pError = trackFace(myDrone, info,w,pid,pError)
     print(info[0][0]) #prints the first element of the tuple representing the center of the face (it will be its cx value)
 
     cv2.imshow('Image',img)
